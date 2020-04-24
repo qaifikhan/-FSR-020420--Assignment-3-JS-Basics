ce31dc17858fc53b62801efed1d26bce5c35f255
Qaifi Khan <qaifikhan@gmail.com>
2020-04-24 18:55:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+// Q.1
+function add_two_digits(n) {
+  //Write your code here
+}
+
+// Q.2
+function angle_Type(angle) {
+  //Write your code here
+}
+
+// Q.3
+function checking_numbers(x, y, divisor) {
+  //Write your code here
+}
+
+// Q.4
+function swap(array) {
+  //Write your code here
+}
+
+// Q.5
+function truncate_string(str1, length) {
+  //Write your code here
+};
+
+// Q.6
+function capitalize_Words(str) {
+  //Write your code here
+}
+
+// Q.7
+function insert(main_string, ins_string, pos) {
+  //Write your code here
+}
+
+// Q.8
+function alphabetize_string(str) {
+  //Write your code here
+}
+
+// Q.9
+function most_frequent_item(arr) {
+  //Write your code here
+}
+
+// Q.10
+function custom_sort(arr) {
+  //Write your code here
+}
+
+// Q.11
+function removeDuplicates(arr) {
+  //Write your code here
+}
+
+// Q.12
+function flatten(arr) {
+  //Write your code here
+}
+
+module.exports = {
+    add_two_digits,
+    angle_Type,
+    checking_numbers,
+    swap,
+    truncate_string,
+    capitalize_Words,
+    insert,
+    alphabetize_string,
+    most_frequent_item,
+    custom_sort,
+    removeDuplicates,
+    flatten
+}
